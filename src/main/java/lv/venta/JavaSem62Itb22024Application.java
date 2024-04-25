cfe1f06b497f0cd0130e1d365e741e202de32035
localuser <localuser@VEA.LV>
2024-04-25 15:13:34 +0300

    initial: Sem6

---
+++ src/main/java/lv/venta/JavaSem62Itb22024Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSem62Itb22024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSem62Itb22024Application.class, args);
+	}
+
+}
