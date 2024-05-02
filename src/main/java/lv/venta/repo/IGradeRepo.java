a061ec8b732edd0e01a927511d04293af501ed84
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-02 15:53:00 +0300

    Add: Service impl for garde filter (not completed)

--- src/main/java/lv/venta/repo/IGradeRepo.java
+++ src/main/java/lv/venta/repo/IGradeRepo.java
@@ -1,9 +1,15 @@
 package lv.venta.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.model.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Integer>{
+
+	ArrayList<Grade> findByStudentIdS(int id);
+
+	ArrayList<Grade> findByStudentIdSAndGrvalueLessThan(int id, int i);
 
 }
---
+++ src/main/java/lv/venta/service/impl/GradeFilterServiceImpl.java
@@ -0,0 +1,54 @@
+package lv.venta.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.model.Grade;
+import lv.venta.repo.IGradeRepo;
+import lv.venta.repo.IStudentRepo;
+import lv.venta.service.IGradeFilterService;
+
+@Service
+public class GradeFilterServiceImpl implements IGradeFilterService{
+
+	@Autowired
+	private IGradeRepo gradeRepo;
+	
+	@Autowired
+	private IStudentRepo studRepo;
+	
+	@Override
+	public ArrayList<Grade> selectGradesByStudentId(int id) throws Exception {
+		if(id <= 0) throw new Exception("Id should be positive");
+		
+		if(!studRepo.existsById(id)) throw new Exception("Student with id (" + id + ") doesn't exist");
+		
+		ArrayList<Grade> result = gradeRepo.findByStudentIdS(id);
+		
+		if(result.isEmpty()) throw new Exception("There is no linkage between this student and grades");		
+		
+		return result;
+	}
+
+	@Override
+	public ArrayList<Grade> selectFailedGradesByStudentId(int id) throws Exception {
+		if(id <= 0) throw new Exception("Id should be positive");
+		
+		if(!studRepo.existsById(id)) throw new Exception("Student with id (" + id + ") doesn't exist");
+
+		ArrayList<Grade> result = gradeRepo.findByStudentIdSAndGrvalueLessThan(id, 4);
+		
+		if(result.isEmpty()) throw new Exception("There is no failed grade for this student");		
+		
+		return result;
+	}
+
+	@Override
+	public float calculateAVGGradeInCourseById(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
